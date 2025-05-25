ee37f11457d1a05f3a04193d94baee1b366b786c
Kyrylo Hrymailo <kirill.grim@gmail.com>
2025-05-25 13:27:53 +0200

    Fixes for deployment

--- chat_client/__main__.py
+++ chat_client/__main__.py
@@ -31,10 +31,14 @@ import uvicorn
 from chat_client.wsgi import app
 
 
-if __name__ == "__main__":
+def main():
     uvicorn.run(
         app=app,
         host=os.environ.get("HOST", "127.0.0.1"),
         port=int(os.environ.get("PORT", 8001)),
         log_level=os.environ.get("LOG_LEVEL", "INFO").lower(),
     )
+
+
+if __name__ == "__main__":
+    main()
